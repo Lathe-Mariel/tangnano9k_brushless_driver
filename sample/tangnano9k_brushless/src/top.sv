07a6caf882b905962148b57b3d5c5a820ac7e9b8
Lathe <nagai@cqpub.co.jp>
2023-09-17 17:09:35 +0900

    0917

--- sample/tangnano9k_brushless/src/top.sv
+++ sample/tangnano9k_brushless/src/top.sv
@@ -5,34 +5,50 @@ module top (
 
   input  wire sw1,
   input  wire sw2,
-  output  wire HIN_R,
-  output  wire HIN_S,
-  output  wire HIN_T,
-  input  wire H_U,
-  input  wire H_V,
-  input  wire H_W,
+  output logic HIN_R,
+  output logic HIN_S,
+  output logic HIN_T,
+  input  wire[2:0] HS,  //HallSensor
   output wire[5:0] boardLED
 );
 
-  logic       oneSecondCLK;
-  logic       oneSecondOverflow;
+  logic       controlCLK;
+  logic  rotateState;
 
 
   timer #(
-    .COUNT_MAX(27000000/2)
+    .COUNT_MAX()
   ) inst_1 (
     .clk (clk),
-    .overflow(oneSecondOverflow)
+    .overflow(controlCLK)
   );
 
-  always @(posedge oneSecondOverflow)begin
-    oneSecondCLK = oneSecondCLK + 1;
+  always @(posedge controlCLK)begin
+    case(rotateState)
+      3'd0: begin HIN_R <= 1; HIN_S <= 0; HIN_T <= 0; end
+      3'd1: begin HIN_R <= 1; HIN_S <= 0; HIN_T <= 0; end
+      3'd2: begin HIN_R <= 0; HIN_S <= 1; HIN_T <= 0; end
+      3'd3: begin HIN_R <= 0; HIN_S <= 1; HIN_T <= 0; end
+      3'd4: begin HIN_R <= 0; HIN_S <= 0; HIN_T <= 1; end
+      3'd5: begin HIN_R <= 0; HIN_S <= 0; HIN_T <= 1; end
+    endcase
+    if(rotateState == 3'd5)begin
+      rotateState <= 3'd0;
+    end else begin
+      rotateState <= rotateState + 3'd1;
+    end
+
+
   end
+
+  assign boardLED[0] = HS[0];
+  assign boardLED[1] = HS[1];
+  assign boardLED[2] = HS[2];
 
 endmodule
 
 module timer #(
-  parameter COUNT_MAX = 27000000
+  parameter COUNT_MAX = 270000
 ) (
   input  wire  clk,
   output logic overflow
