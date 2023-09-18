2a3ef95a94695d64b4af21d6fbaf24203c83d8a6
Lathe <n11akira@yahoo.co.jp>
2023-09-18 20:48:26 +0900

    0918

--- sample/tangnano9k_brushless/src/top.sv
+++ sample/tangnano9k_brushless/src/top.sv
@@ -5,15 +5,22 @@ module top (
 
   input  wire sw1,
   input  wire sw2,
+  input wire[3:0] tacSW,
+  input wire[2:0] toggleSW,
+  input  wire[2:0] HS,  //HallSensor
   output logic HIN_R,
   output logic HIN_S,
   output logic HIN_T,
-  input  wire[2:0] HS,  //HallSensor
+  output logic _LIN_R,
+  output logic _LIN_S,
+  output logic _LIN_T,
+  output logic[7:0] anode,
+  output logic[3:0] cathode,
   output wire[5:0] boardLED
 );
 
-  logic       controlCLK;
-  logic  rotateState;
+  logic  controlCLK;
+  logic[2:0]  rotateState;
 
 
   timer #(
@@ -24,26 +31,31 @@ module top (
   );
 
   always @(posedge controlCLK)begin
-    case(rotateState)
-      3'd0: begin HIN_R <= 1; HIN_S <= 0; HIN_T <= 0; end
-      3'd1: begin HIN_R <= 1; HIN_S <= 0; HIN_T <= 0; end
-      3'd2: begin HIN_R <= 0; HIN_S <= 1; HIN_T <= 0; end
-      3'd3: begin HIN_R <= 0; HIN_S <= 1; HIN_T <= 0; end
-      3'd4: begin HIN_R <= 0; HIN_S <= 0; HIN_T <= 1; end
-      3'd5: begin HIN_R <= 0; HIN_S <= 0; HIN_T <= 1; end
-    endcase
+    if(toggleSW[2])begin
+      case(rotateState)
+        3'd0: begin HIN_R <= 1; _LIN_R <= 1; HIN_S <= 0; _LIN_S <= 0; HIN_T <= 0; _LIN_T <= 1; end
+        3'd1: begin HIN_R <= 1; _LIN_R <= 1; HIN_S <= 0; _LIN_S <= 1; HIN_T <= 0; _LIN_T <= 0; end
+        3'd2: begin HIN_R <= 0; _LIN_R <= 1; HIN_S <= 1; _LIN_S <= 1; HIN_T <= 0; _LIN_T <= 0; end
+        3'd3: begin HIN_R <= 0; _LIN_R <= 0; HIN_S <= 1; _LIN_S <= 1; HIN_T <= 0; _LIN_T <= 1; end
+        3'd4: begin HIN_R <= 0; _LIN_R <= 0; HIN_S <= 0; _LIN_S <= 1; HIN_T <= 1; _LIN_T <= 1; end
+        3'd5: begin HIN_R <= 0; _LIN_R <= 1; HIN_S <= 0; _LIN_S <= 0; HIN_T <= 1; _LIN_T <= 1; end
+      endcase
+    end else begin
+      HIN_R <= 0; _LIN_R <= 0; HIN_S <= 0; _LIN_S <= 0; HIN_T <= 0; _LIN_T <= 0;
+    end
     if(rotateState == 3'd5)begin
       rotateState <= 3'd0;
     end else begin
       rotateState <= rotateState + 3'd1;
     end
 
-
   end
 
-  assign boardLED[0] = HS[0];
-  assign boardLED[1] = HS[1];
-  assign boardLED[2] = HS[2];
+  assign boardLED[2:0] = HS;
+  assign boardLED[5:3] = toggleSW;
+//test
+  assign anode = 8'b00010100;
+  assign cathode = 4'b0001;
 
 endmodule
 
