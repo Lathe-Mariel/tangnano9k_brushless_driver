c4a83cdc108078ba4656cdaa45be464bf7b67a17
Lathe <nagai@cqpub.co.jp>
2023-09-19 17:16:15 +0900

    0919

--- sample/tangnano9k_brushless/src/top.sv
+++ sample/tangnano9k_brushless/src/top.sv
@@ -20,7 +20,13 @@ module top (
 );
 
   logic  controlCLK;
+  logic rotateCLK;
+  logic[3:0] rotateCounter;
   logic[2:0]  rotateState;
+  logic duty;
+  logic _LR;
+  logic _LS;
+  logic _LT;
 
 
   timer #(
@@ -31,17 +37,31 @@ module top (
   );
 
   always @(posedge controlCLK)begin
+    if(rotateCounter == 0)begin
+      rotateCLK <= ~rotateCLK;
+    end
+    rotateCounter <= rotateCounter + 'b1;
+
+    if(rotateCounter[1:0] == 2'b11)begin
+      duty <= 'b1;
+    end else begin
+      duty <= 'b0;
+    end
+
+  end
+
+  always @(posedge rotateCLK)begin
     if(toggleSW[2])begin
       case(rotateState)
-        3'd0: begin HIN_R <= 1; _LIN_R <= 1; HIN_S <= 0; _LIN_S <= 0; HIN_T <= 0; _LIN_T <= 1; end
-        3'd1: begin HIN_R <= 1; _LIN_R <= 1; HIN_S <= 0; _LIN_S <= 1; HIN_T <= 0; _LIN_T <= 0; end
-        3'd2: begin HIN_R <= 0; _LIN_R <= 1; HIN_S <= 1; _LIN_S <= 1; HIN_T <= 0; _LIN_T <= 0; end
-        3'd3: begin HIN_R <= 0; _LIN_R <= 0; HIN_S <= 1; _LIN_S <= 1; HIN_T <= 0; _LIN_T <= 1; end
-        3'd4: begin HIN_R <= 0; _LIN_R <= 0; HIN_S <= 0; _LIN_S <= 1; HIN_T <= 1; _LIN_T <= 1; end
-        3'd5: begin HIN_R <= 0; _LIN_R <= 1; HIN_S <= 0; _LIN_S <= 0; HIN_T <= 1; _LIN_T <= 1; end
+        3'd0: begin HIN_R <= 1; _LR <= 1; HIN_S <= 0; _LS <= 0; HIN_T <= 0; _LT <= 1; end
+        3'd1: begin HIN_R <= 1; _LR <= 1; HIN_S <= 0; _LS <= 1; HIN_T <= 0; _LT <= 0; end
+        3'd2: begin HIN_R <= 0; _LR <= 1; HIN_S <= 1; _LS <= 1; HIN_T <= 0; _LT <= 0; end
+        3'd3: begin HIN_R <= 0; _LR <= 0; HIN_S <= 1; _LS <= 1; HIN_T <= 0; _LT <= 1; end
+        3'd4: begin HIN_R <= 0; _LR <= 0; HIN_S <= 0; _LS <= 1; HIN_T <= 1; _LT <= 1; end
+        3'd5: begin HIN_R <= 0; _LR <= 1; HIN_S <= 0; _LS <= 0; HIN_T <= 1; _LT <= 1; end
       endcase
     end else begin
-      HIN_R <= 0; _LIN_R <= 0; HIN_S <= 0; _LIN_S <= 0; HIN_T <= 0; _LIN_T <= 0;
+      HIN_R <= 0; _LR <= 0; HIN_S <= 0; _LS <= 0; HIN_T <= 0; _LT <= 0;
     end
     if(rotateState == 3'd5)begin
       rotateState <= 3'd0;
@@ -53,6 +73,11 @@ module top (
 
   assign boardLED[2:0] = HS;
   assign boardLED[5:3] = toggleSW;
+
+  assign _LIN_R = ~(~_LR * duty);
+  assign _LIN_S = ~(~_LS * duty);
+  assign _LIN_T = ~(~_LT * duty);
+
 //test
   assign anode = 8'b00010100;
   assign cathode = 4'b0001;
